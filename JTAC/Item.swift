297cbc42b866eab022eaee6c50a25fd716bb096e
shyver <eloued.abdelwahed@gmail.com>
2026-01-27 22:34:53 +0000

    first commit

---
+++ JTAC/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  JTAC
+//
+//  Created by admin on 1/27/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
